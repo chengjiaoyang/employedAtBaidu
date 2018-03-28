cce8bf9ecb369ca3419f645135d67d9787b3f803
chengjiaoyang <33946339+chengjiaoyang@users.noreply.github.com>
2018-03-28 15:28:13 +0800

    2018-3-24

---
+++ employedAtBaidu/chapter1/01.js
@@ -0,0 +1,20 @@
+1 上来就是几个没听说过的名词解释
+mod.JS  对比 require.js
+
+ modJS是百度fex-team提供的一个轻量级的模块加载器，类似requirejs。
+但modJS并不完全兼容规范amd/cmd，事实上，只支持非常简单的全局方法define(id,factory)。
+
+另外factory提供了3个参数require/exports/module，用于引用和导出模块。
+
+
+
+yog2 
+
+
+
+swig 对比 ejs  jade
+
+
+
+bigpipe   对比 ajax`
+`
